3d647980eedff33d1ad53037ec4c1ea1f85bc1f0
Preeti.Singh <Preeti.Singh@ASIA29120>
2020-11-11 16:45:28 +0530

    homepage body

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./menu-item.style.scss";
+const MenuItems = ({ title, imageUrl, size }) => {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItems;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
