1c6c64879f9dccc9f0575e6b8335bd1a76095c81
Preeti.Singh <Preeti.Singh@ASIA29120>
2020-11-25 19:12:22 +0530

    redux implementation

--- src/App.js
+++ src/App.js
@@ -6,32 +6,27 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { Route, Switch } from "react-router-dom";
 import Header from './components/header/header.component';
 import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
+import{connect} from "react-redux";
+import{setCurrentUser} from "./redux/user/user.action";
+
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubsscribeafromAuth=null;
 
   componentDidMount() {
+    const{setCurrentUser}=this.props
     this.unsubsscribeafromAuth=auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef=await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShort=>{
-          this.setState({
-            currentUser:{
+          setCurrentUser({
               id:snapShort.id,
-              ...snapShort.data()
-            }
+              ...snapShort.data()            
           })
         })
       }
-      this.setState({currentUser:userAuth})
+      setCurrentUser(userAuth)
     })
   }
 
@@ -42,7 +37,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -53,4 +48,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps=dispatch=>({
+  setCurrentUser:user=>dispatch(setCurrentUser(user))
+})
+
+export default connect(null,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from "./App"
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./redux/store";
+
 
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
